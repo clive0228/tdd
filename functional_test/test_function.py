9647b17a9beb207eb0317f309721fedc4ccc2b04
clive0228 <clive0228@gmail.com>
2015-11-15 19:48:25 +0900

    pytest, flake8 test complete

--- functional_test/test_function.py
+++ functional_test/test_function.py
@@ -1,23 +1,24 @@
 from selenium import webdriver
 import unittest
 
-class NewVisitorTest(unittest.TestCase) :
 
-	def setUp(self) :
-		self.browser = webdriver.Chrome()		
+class NewVisitorTest(unittest.TestCase):
 
-	def tearDown(self) :
-		self.browser.quit()
+    def setUp(self):
+        self.browser = webdriver.Chrome()
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		#connecting website
-		self.browser.get('http://localhost:8000')
+    def tearDown(self):
+        self.browser.quit()
 
-		#check title of website
-		self.assertIn('To-Do',self.browser.title)
-		self.fail("Test Finished!")
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # connecting website
+        self.browser.get('http://localhost:8000')
 
-		#terminate website
+        # check title of website
+        self.assertIn('To-Do', self.browser.title)
+        self.fail("Test Finished!")
+
+        # terminate website
 
 if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+    unittest.main(warnings='ignore')
--- lists/test_lists.py
+++ lists/test_lists.py
@@ -1,6 +1,17 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 
-class SmokeTest(TestCase):
+from lists.views import home_page
 
-	def test_bad_mouth(self):
-		self.assertEqual(1+1,3)
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
