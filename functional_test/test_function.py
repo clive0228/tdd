d6a4e0e17527c5bc8c3f167b21f2a605a1e32f6e
clive0228 <clive0228@gmail.com>
2015-11-15 21:05:41 +0900

    correct flake error

--- functional_test/test_function.py
+++ functional_test/test_function.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 
 import unittest
-import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,7 +15,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # connecting website
         self.browser.get('http://localhost:8000')
-        
+
         # check title of website
         self.assertIn('To-Do', self.browser.title)
         # self.fail("Test Finished!")
